1da8df520655044997ad301e528ac474f03fedc4
BigTwisty <bigtwisty@gmail.com>
2013-09-10 15:06:09 -0400

    Added filelist

--- install.lua
+++ install.lua
@@ -1,6 +1,3 @@
-h = http.get("https://api.github.com/repos/BigTwisty/TLogic/blob/working/files.txt")
-if not h then error("Could not load working files.", 0) end
-
 local function assert(value, message)
   if not value then error(message, 0)
   return value
@@ -9,5 +6,14 @@ end
 if fs.exists("TLogic") then fs.delete("/TLogic") end
 fs.makeDir("TLogic")
 
+
 function git(path)
-  h = 
+  h = http.get("https://raw.github.com/BigTwisty/TLogic/working/"..path)
+  if not h then return false end
+  file = h.readAll()
+  h.close()
+  return h
+end
+
+files = assert(git("filelist.txt"), "Error downloading filelist.txt from github")
+print(files)
